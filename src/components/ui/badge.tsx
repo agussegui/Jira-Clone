b1df6969483394e38cb14e8147d6c01d9b81d925
AgustinSegui <agussegui24@hotmail.es>
2025-01-28 00:48:27 -0300

    feat:change a different colors in the select tabs and button. badge add classes for taskstatus with different colors

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { TaskStatus } from "@/features/tasks/types"
 
 const badgeVariants = cva(
   "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -15,6 +16,16 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
         outline: "text-foreground",
+        [TaskStatus.HACER]:
+          "border-transparent bg-red-500 text-primary hover:bg-red-500/80 text-white",
+        [TaskStatus.EN_PROGRESO]:
+          "border-transparent bg-yellow-500 text-primary hover:bg-red-500/80 text-white",  
+        [TaskStatus.EN_REVISION]:
+          "border-transparent bg-blue-500 text-primary hover:bg-red-500/80 text-white",
+        [TaskStatus.TERMINADO]:
+          "border-transparent bg-green-700 text-primary hover:bg-red-700/80 text-white",
+        [TaskStatus.PENDIENTE]:
+          "border-transparent bg-pink-500 text-primary hover:bg-red-500/80 text-white",
       },
     },
     defaultVariants: {
