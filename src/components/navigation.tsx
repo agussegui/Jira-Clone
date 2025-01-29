c8c92662e340207be75319f0082491bc255b058a
AgustinSegui <agussegui24@hotmail.es>
2025-01-29 19:18:54 -0300

    feat: add navigation component with dynamic routes
    - Implemented the Navigation component that generates a menu based on dynamic routes.
    - Uses useWorkspaceId to generate workspace-specific routes.
    - Determines the active route state using usePathname.

---
+++ src/components/navigation.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { SettingsIcon, UsersIcon } from "lucide-react";
+import Link from "next/link";
+import {GoCheckCircle, GoCheckCircleFill, GoHome, GoHomeFill} from "react-icons/go"
+
+
+const routes = [
+    {
+        label: "Inicio",
+        href: "",
+        icon: GoHome,
+        activeIcon: GoHomeFill,
+    },
+    {
+        label: "Mis Tareas",
+        href: "/tasks",
+        icon: GoCheckCircle,
+        activeIcon: GoCheckCircleFill,
+    },
+    {
+        label: "Ajustes",
+        href: "/settings",
+        icon: SettingsIcon,
+        activeIcon: SettingsIcon,
+    },
+    {
+        label: "Miembros",
+        href: "/members",
+        icon: UsersIcon,
+        activeIcon: UsersIcon,
+    },
+]
+
+
+const Navigation = () => {
+
+    const workspaceId = useWorkspaceId();
+    const pathname = usePathname();
+
+    return (  
+        <ul className="flex flex-col ">
+            {routes.map((item) => {
+                const fullHref = `/workspaces/${workspaceId}${item.href}`
+                const isActive = pathname === fullHref;
+                const Icon = isActive ? item.activeIcon : item.icon;
+                return (
+                    <Link key={item.href} href={fullHref}>
+                        <div className={cn("flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500", isActive && 'bg-white shadow-sm hover:opacity-100 text-primary')}>
+                            <Icon className="size-5 text-neutral-500"/>
+                            {item.label}
+                        </div>
+                    </Link>
+                )
+            })}
+        </ul>
+    );
+}
+ 
+export default Navigation;
