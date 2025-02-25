12bb703a1821e5310dbddd77e1283c99cf9a1a8e
AgustinSegui <agussegui24@hotmail.es>
2025-02-25 16:28:49 -0300

    feat: add TaskIdClient with task details, breadcrumbs, and overview

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { DottedSeparator } from "@/components/dotted-sepator";
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { TaskBreadcrums } from "@/features/tasks/components/task-breadcrums";
+import { TaskDescription } from "@/features/tasks/components/task-description";
+import { TaskOverview } from "@/features/tasks/components/task-overview";
+import { useTaskId } from "@/features/tasks/hooks/use-task-id";
+
+export const TaskIdClient = () => {
+
+    const taskId = useTaskId();
+    const {data, isLoading} = useGetTask({taskId})
+
+    if(isLoading){
+        return <PageLoader/>
+    }
+
+    if(!data){
+        return <PageError message="Tarea no encontrada"/>
+    }
+
+    return (
+        <div className="flex flex-col ">
+            <TaskBreadcrums project={data.project} task={data}/>
+            <DottedSeparator className="my-6"/>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <TaskOverview task={data}/>
+                <TaskDescription task={data}/>
+            </div>
+
+        </div>
+    )
+} 
