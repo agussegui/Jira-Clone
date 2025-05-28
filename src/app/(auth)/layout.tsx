5ca0df671b70e07dad09c7da4ce3d5bdbb9dcf24
AgustinSegui <agussegui24@hotmail.es>
2025-05-28 14:57:32 -0300

    feat:delete a AuthLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,23 +1,23 @@
-'use client';
-import Image from 'next/image';
-import React from 'react';
+// 'use client';
+// import Image from 'next/image';
+// import React from 'react';
 
-interface AuthLayoutProps {
-  children: React.ReactNode;
-}
-const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return (
-    <main className="bg-neutral-100 min-h-screen">
-      <div className="mx-auto max-w-screen-2xl p-4">
-        <nav className="flex justify-between items-center">
-          <Image src="/logotask5.png" alt="logo" width={300} height={156} />
-        </nav>
-        <div className="flex flex-col items-center justify-center ">
-          {children}
-        </div>
-      </div>
-    </main>
-  );
-};
+// interface AuthLayoutProps {
+//   children: React.ReactNode;
+// }
+// const AuthLayout = ({ children }: AuthLayoutProps) => {
+//   return (
+//     <main className="bg-neutral-100 min-h-screen">
+//       <div className="mx-auto max-w-screen-2xl p-4">
+//         <nav className="flex justify-between items-center">
+//           <Image src="/logotask5.png" alt="logo" width={300} height={156} />
+//         </nav>
+//         <div className="flex flex-col items-center justify-center ">
+//           {children}
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
 
-export default AuthLayout;
+// export default AuthLayout;
