a5b05ef7978cc098b7f7f9f81c66069146c86ef2
AgustinSegui <agussegui24@hotmail.es>
2025-05-28 15:09:50 -0300

    add a new layout and delete the outh authorization in the oauth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,23 +1,34 @@
-// 'use client';
-// import Image from 'next/image';
-// import React from 'react';
+'use client';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
-// interface AuthLayoutProps {
-//   children: React.ReactNode;
-// }
-// const AuthLayout = ({ children }: AuthLayoutProps) => {
-//   return (
-//     <main className="bg-neutral-100 min-h-screen">
-//       <div className="mx-auto max-w-screen-2xl p-4">
-//         <nav className="flex justify-between items-center">
-//           <Image src="/logotask5.png" alt="logo" width={300} height={156} />
-//         </nav>
-//         <div className="flex flex-col items-center justify-center ">
-//           {children}
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logotask5.png" alt="logo" width={300} height={156} />
+          <div className="flex items-center gap-2">
+            <Button asChild variant="secondary">
+              <Link href={pathname === '/sign-in' ? '/sign-up' : '/sign-in'}>
+                {pathname === '/sign-in' ? 'Registrarse' : 'Iniciar Sesion'}
+              </Link>
+            </Button>
+          </div>
+        </nav>
+        <div className="flex flex-col items-center justify-center ">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
 
-// export default AuthLayout;
+export default AuthLayout;
