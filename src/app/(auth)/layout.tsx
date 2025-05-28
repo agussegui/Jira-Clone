0790b0ce9397b42d99c7e14c5d067f459ee8fdb2
AgustinSegui <agussegui24@hotmail.es>
2025-05-28 14:56:03 -0300

    feat:change a AuthLayout remove the button sign up

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,27 +1,16 @@
 'use client';
-import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import React from 'react';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  const pathname = usePathname();
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logotask5.png" alt="logo" width={300} height={156} />
-          <div className="flex items-center gap-2">
-            <Button asChild variant="secondary">
-              <Link href={pathname === '/sign-in' ? '/sign-up' : '/sign-in'}>
-                {pathname === '/sign-in' ? 'Registrarse' : 'Iniciar Sesion'}
-              </Link>
-            </Button>
-          </div>
         </nav>
         <div className="flex flex-col items-center justify-center ">
           {children}
